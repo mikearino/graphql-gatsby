a131c35690432b8303a2aa4316d26fd787900246
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-18 22:07:26 -0800

    simple graphql demo.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,16 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
